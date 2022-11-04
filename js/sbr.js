e4cfe75b2f6a768fbd65872f4ab0e3e24d98844b
Zacks <53922473+Zackperez@users.noreply.github.com>
2022-11-04 16:56:02 -0500

    Add files via upload

--- js/sbr.js
+++ js/sbr.js
@@ -6,15 +6,51 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     function mostrar_pantalla(){
-        const abdominal = document.getElementById("pregunta_abdominal").value;
-        const diarrea = document.getElementById("pregunta_diarrea").value;
-        const estrenimiento = document.getElementById("pregunta_estrenimiento").value;
-        const acidez = document.getElementById("pregunta_acidez").value;
-        const vomito = document.getElementById("pregunta_vomitos").value;
-        console.log(abdominal);
-        console.log(diarrea);
-        console.log(estrenimiento);
-        console.log(acidez);
-        console.log(vomito);
+        const abdominal = document.getElementById("pregunta_abdominal");
+        const diarrea = document.getElementById("pregunta_diarrea");
+        const estrenimiento = document.getElementById("pregunta_estrenimiento");
+        const acidez = document.getElementById("pregunta_acidez");
+        const vomito = document.getElementById("pregunta_vomitos");
+
+
+        if (abdominal.value == ""){
+            abdominal.classList.remove('is-valid');
+            abdominal.classList.add('is-invalid');
+        }else{
+            abdominal.classList.remove('is-invalid');
+            abdominal.classList.add('is-valid');
+        }
+
+        if (diarrea.value == ""){
+            diarrea.classList.remove('is-valid');
+            diarrea.classList.add('is-invalid');
+        }else{
+            diarrea.classList.remove('is-invalid');
+            diarrea.classList.add('is-valid');
+        }
+
+        if (estrenimiento.value == ""){
+            estrenimiento.classList.remove('is-valid');
+            estrenimiento.classList.add('is-invalid');
+        }else{
+            estrenimiento.classList.remove('is-invalid');
+            estrenimiento.classList.add('is-valid');
+        }
+
+        if (acidez.value == ""){
+            acidez.classList.remove('is-valid');
+            acidez.classList.add('is-invalid');
+        }else{
+            acidez.classList.remove('is-invalid');
+            acidez.classList.add('is-valid');
+        }
+
+        if (vomito.value == ""){
+            vomito.classList.remove('is-valid');
+            vomito.classList.add('is-invalid');
+        }else{
+            vomito.classList.remove('is-invalid');
+            vomito.classList.add('is-valid');
+        }
     }
 });
