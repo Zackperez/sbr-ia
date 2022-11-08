0151e2cc6561b4023e21578ed2d119e0cafa0ebb
Zacks <53922473+Zackperez@users.noreply.github.com>
2022-11-07 23:58:06 -0500

    Add files via upload

--- js/sbr.js
+++ js/sbr.js
@@ -1,10 +1,41 @@
 document.addEventListener('DOMContentLoaded', function(){
     const boton_prueba = document.getElementById("boton_prueba");
-    
-    boton_prueba.onclick = function(){
-        mostrar_pantalla();
+
+    /*function add_question(){
+        const abdominal = document.getElementById("pregunta_abdominal").value;
+        const diarrea = document.getElementById("pregunta_diarrea").value;
+        const estrenimiento = document.getElementById("pregunta_estrenimiento").value;
+        const acidez = document.getElementById("pregunta_acidez").value;
+        const vomito = document.getElementById("pregunta_vomitos").value;
+        
+        const respuestas_preguntas = {
+            respuesta_1: abdominal,
+            respuesta_2: diarrea,
+            respuesta_3: estrenimiento,
+            respuesta_4: acidez,
+            respuesta_5: vomito
+        }
+    }*/
+
+    function getAll(){
+        axios.get('http://localhost:4000/get_user_info')
+        .then (function (response){
+            console.log(response.data[0]);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+
     }
 
+
+    
+    boton_prueba.onclick = function(){
+        getAll();
+    }
+
+
+/*
     function mostrar_pantalla(){
         const abdominal = document.getElementById("pregunta_abdominal");
         const diarrea = document.getElementById("pregunta_diarrea");
@@ -19,6 +50,7 @@ document.addEventListener('DOMContentLoaded', function(){
         }else{
             abdominal.classList.remove('is-invalid');
             abdominal.classList.add('is-valid');
+            console.log(abdominal.value);
         }
 
         if (diarrea.value == ""){
@@ -27,6 +59,7 @@ document.addEventListener('DOMContentLoaded', function(){
         }else{
             diarrea.classList.remove('is-invalid');
             diarrea.classList.add('is-valid');
+            console.log(diarrea.value);
         }
 
         if (estrenimiento.value == ""){
@@ -35,6 +68,7 @@ document.addEventListener('DOMContentLoaded', function(){
         }else{
             estrenimiento.classList.remove('is-invalid');
             estrenimiento.classList.add('is-valid');
+            console.log(estrenimiento.value);
         }
 
         if (acidez.value == ""){
@@ -43,6 +77,7 @@ document.addEventListener('DOMContentLoaded', function(){
         }else{
             acidez.classList.remove('is-invalid');
             acidez.classList.add('is-valid');
+            console.log(acidez.value);
         }
 
         if (vomito.value == ""){
@@ -51,6 +86,7 @@ document.addEventListener('DOMContentLoaded', function(){
         }else{
             vomito.classList.remove('is-invalid');
             vomito.classList.add('is-valid');
+            console.log(vomito.value);
         }
-    }
+    }*/
 });
