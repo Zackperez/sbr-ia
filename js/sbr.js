307456c6439d6a56f3d04096cdf91091970fa760
Zacks <53922473+Zackperez@users.noreply.github.com>
2022-11-22 13:21:46 -0500

    Add files via upload

--- js/sbr.js
+++ js/sbr.js
@@ -1,121 +1,87 @@
 document.addEventListener('DOMContentLoaded', function () {
+    //Botones
     const boton_prueba = document.getElementById("boton_prueba");
+    const boton_limpiar = document.getElementById("boton_limpiar");
     const boton_resultados = document.getElementById("boton_resultados");
 
+    //Campos del formulario
+    const Id_usuario = document.getElementById("id_usuario");
+    const Respuesta_nombre = document.getElementById("respuesta_nombre");
+    const Respuesta_apellido = document.getElementById("respuesta_apellido");
+    const Respuesta_abdominal = document.getElementById("respuesta_abdominal");
+    const Respuesta_diarrea = document.getElementById("respuesta_diarrea");
+    const Respuesta_estrenimiento = document.getElementById("respuesta_estrenimiento");
+    const Respuesta_acidez = document.getElementById("respuesta_acidez");
+    const Respuesta_vomitos = document.getElementById("respuesta_vomitos");
 
-    function new_question() {
-        const abdominal = document.getElementById("pregunta_abdominal").value;
-        const diarrea = document.getElementById("pregunta_diarrea").value;
-        const estrenimiento = document.getElementById("pregunta_estrenimiento").value;
-        const acidez = document.getElementById("pregunta_acidez").value;
-        const vomito = document.getElementById("pregunta_vomitos").value;
+    function borrar_campos() {
 
-        const respuestas_preguntas = {
-            Respuesta_1: abdominal,
-            Respuesta_2: diarrea,
-            Respuesta_3: estrenimiento,
-            Respuesta_4: acidez,
-            Respuesta_5: vomito
-        }
-        axios({
-            method: "POST",
-            url: "http://localhost:4000/add_contact",
-            data: respuestas_preguntas,
-        })
-            .then(res => console.log(res))
-            .catch(err => console.log('Error:', err))
+        Id_usuario.value = "";
+        Respuesta_nombre.value = "";
+        Respuesta_apellido.value = "";
+        Id_usuario.classList.remove("is-invalid");
+        Respuesta_nombre.classList.remove("is-invalid");
+        Respuesta_apellido.classList.remove("is-invalid");
+        Respuesta_abdominal.classList.remove("is-invalid");
+        Respuesta_diarrea.classList.remove("is-invalid");
+        Respuesta_estrenimiento.classList.remove("is-invalid");
+        Respuesta_acidez.classList.remove("is-invalid");
+        Respuesta_vomitos.classList.remove("is-invalid");
+
+        Id_usuario.classList.remove("is-valid");
+        Respuesta_nombre.classList.remove("is-valid");
+        Respuesta_apellido.classList.remove("is-valid");
+        Respuesta_abdominal.classList.remove("is-valid");
+        Respuesta_diarrea.classList.remove("is-valid");
+        Respuesta_estrenimiento.classList.remove("is-valid");
+        Respuesta_acidez.classList.remove("is-valid");
+        Respuesta_vomitos.classList.remove("is-valid");
+
+    }
+
+    boton_limpiar.onclick = function () {
+        borrar_campos();
     }
 
     function new_user() {
-        const Id_usuario = document.getElementById("id_usuario").value;
-        const Respuesta_usuario = document.getElementById("respuesta_usuario").value;
-        const Respuesta_apellido = document.getElementById("respuesta_apellido").value;
-        const Respuesta_abdominal = document.getElementById("respuesta_abdominal").value;
-        const Respuesta_diarrea = document.getElementById("respuesta_diarrea").value;
-        const Respuesta_estrenimiento = document.getElementById("respuesta_estrenimiento").value;
-        const Respuesta_acidez = document.getElementById("respuesta_acidez").value;
-        const Respuesta_vomitos = document.getElementById("respuesta_vomitos").value;
-        const resultados = document.getElementById("resultados_finales");
-        const parrafo_resultados = document.getElementById("parrafo_resultados");
 
-        const respuestas_preguntas = {
-            Id_usuario: Id_usuario,
-            Nombre: Respuesta_usuario,
-            Apellido: Respuesta_apellido,
-            Respuesta_abdominal: Respuesta_abdominal,
-            Respuesta_diarrea: Respuesta_diarrea,
-            Respuesta_estrenimiento: Respuesta_estrenimiento,
-            Respuesta_acidez: Respuesta_acidez,
-            Respuesta_vomitos: Respuesta_vomitos
+        !Id_usuario.value.match(/^[0-9]+$/) ? Id_usuario.classList.add("is-invalid") : Id_usuario.classList.remove("is-invalid"); Id_usuario.classList.add("is-valid");
+        !Respuesta_nombre.value.match(/^[a-zA-Z]+$/) ? Respuesta_nombre.classList.add("is-invalid") : Respuesta_nombre.classList.remove("is-invalid"); Respuesta_nombre.classList.add("is-valid");
+        !Respuesta_apellido.value.match(/^[a-zA-Z]+$/) ? Respuesta_apellido.classList.add("is-invalid") : Respuesta_apellido.classList.remove("is-invalid"); Respuesta_apellido.classList.add("is-valid");
+        Respuesta_abdominal.value == "" ? Respuesta_abdominal.classList.add("is-invalid") : Respuesta_abdominal.classList.remove("is-invalid"); Respuesta_abdominal.classList.add("is-valid");
+        Respuesta_diarrea.value == "" ? Respuesta_diarrea.classList.add("is-invalid") : Respuesta_diarrea.classList.remove("is-invalid"); Respuesta_diarrea.classList.add("is-valid");
+        Respuesta_estrenimiento.value == "" ? Respuesta_estrenimiento.classList.add("is-invalid") : Respuesta_estrenimiento.classList.remove("is-invalid"); Respuesta_estrenimiento.classList.add("is-valid");
+        Respuesta_acidez.value == "" ? Respuesta_acidez.classList.add("is-invalid") : Respuesta_acidez.classList.remove("is-invalid"); Respuesta_acidez.classList.add("is-valid");
+        Respuesta_vomitos.value == "" ? Respuesta_vomitos.classList.add("is-invalid") : Respuesta_vomitos.classList.remove("is-invalid"); Respuesta_vomitos.classList.add("is-valid");
+
+
+/* 
+        if () {
+            
+        }else{
+            const respuestas_preguntas = {
+                Id_usuario: Id_usuario,
+                Nombre: Respuesta_usuario,
+                Apellido: Respuesta_apellido,
+                Respuesta_abdominal: Respuesta_abdominal,
+                Respuesta_diarrea: Respuesta_diarrea,
+                Respuesta_estrenimiento: Respuesta_estrenimiento,
+                Respuesta_acidez: Respuesta_acidez,
+                Respuesta_vomitos: Respuesta_vomitos
+            }
+    
+              axios({
+                method: "POST",
+                url: "http://localhost:4000/agregar_usuario_temporal",
+                data: respuestas_preguntas,
+            })
+                .then(res =>
+                    console.log(res))
+                .catch(err => console.log('Error:', err))
         }
 
-        axios({
-            method: "POST",
-            url: "http://localhost:4000/agregar_usuario_temporal",
-            data: respuestas_preguntas,
-        })
-            .then(res =>
-                console.log(res))
-            .catch(err => console.log('Error:', err))
+        }*/
     }
-
-    function mostrar_resultados() {
-        const Id_usuario = document.getElementById("id_usuario").value;
-        axios.get('http://localhost:4000/respuesta_sbr/' + Id_usuario)
-            .then(function (response) {
-                console.log(response.data[0]);
-                parrafo_resultados.innerHTML = response.data[0];
-                resultados.appendChild(parrafo_resultados);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-
-    function getAll() {
-        axios.get('http://localhost:4000/get_user_info')
-            .then(function (response) {
-                console.log(response.data[0]);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-
-    /*     function new_user(){
-        const Id_usuario = document.getElementById("id_usuario").value;
-        const Respuesta_usuario = document.getElementById("respuesta_usuario").value;
-        const Respuesta_apellido = document.getElementById("respuesta_apellido").value;
-        const Respuesta_abdominal = document.getElementById("respuesta_abdominal").value;
-        const Respuesta_diarrea = document.getElementById("respuesta_diarrea").value;
-        const Respuesta_estrenimiento = document.getElementById("respuesta_estrenimiento").value;
-        const Respuesta_acidez = document.getElementById("respuesta_acidez").value;
-        const Respuesta_vomitos = document.getElementById("respuesta_vomitos").value;
-
-
-        const respuestas_preguntas = {
-            Id_usuario: Id_usuario,
-            Nombre: Respuesta_usuario,
-            Apellido: Respuesta_apellido,
-            Respuesta_abdominal: Respuesta_abdominal,
-            Respuesta_diarrea: Respuesta_diarrea,
-            Respuesta_estrenimiento: Respuesta_estrenimiento,
-            Respuesta_acidez: Respuesta_acidez,
-            Respuesta_vomitos: Respuesta_vomitos
-        }
-        axios({
-            method: "POST",
-            url: "http://localhost:4000/agregar_usuario_temporal",
-            data: respuestas_preguntas,
-        })
-        .then(res => console.log(res)
-            )
-        .catch(err => console.log('Error:',err))
-    }*/
-
-
-
-
 
     function mostrar_respuesta() {
         id_usuario = document.getElementById("id_usuario").value;
@@ -132,33 +98,11 @@ document.addEventListener('DOMContentLoaded', function () {
             });
     }
 
-    /*
-        function mostrar_respuesta(){
-            buscar_id =  document.getElementById("buscar_id").value;
-            let resultados = document.getElementById("resultados_finales");
-            let parrafo_resultados = document.createElement('p');
-            axios.get('http://localhost:4000/respuesta_sbr/'+buscar_id)
-            .then (function (response){
-                console.log(response.data[0]);
-                parrafo_resultados.innerHTML = response.data[0];
-                resultados.appendChild(parrafo_resultados);
-            })
-            .catch(function(error) {
-                console.log(error);
-            });        
-        }
-    */
-
-
     boton_prueba.onclick = function () {
-        /* new_user(); 
-        var selectedOption = document.getElementById("floatingSelect_vomitos").value;
-
-        console.log(selectedOption);*/
         new_user();
     }
 
-    boton_resultados.onclick = function(){
+    boton_resultados.onclick = function () {
         mostrar_respuesta();
     }
 
